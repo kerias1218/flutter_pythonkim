f336773683217db6beb6697fb817608c796a8132
asdf <asdf@asdf.com>
2019-09-16 15:48:33 +0900

    사진 1개 가운데 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() {
     runApp(MaterialApp(
-        title: '텍스트 위젯',
+        title: '사진',
         home: Scaffold(
-            appBar: AppBar(title: Text('수직 정렬'),),
+            appBar: AppBar(title: Text('사진1'),),
             body: MyApp(),
         ),
     ));
@@ -15,43 +15,10 @@ class MyApp extends StatelessWidget
     @override
     Widget build(BuildContext context) {
         // TODO: implement build
-        return Column(
-            children: <Widget>[
-                makeRow(left:'1',middle:'2',right:'3'),
-                makeRow(left:'4',middle:'5',right:'6'),
-                makeRow(left:'7',middle:'8',right:'9'),
-
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        return Center(
+            child: Image.asset('images/001.jpg'),
         );
+
     }
 
-    Widget makeRow({String left, String middle, String right}) {
-        return Row(
-            children: <Widget>[
-                makeText(left, width: 100, height: 100),
-                makeText(middle, width: 100, height: 100),
-                makeText(right, width: 100, height: 100),
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-        );
-    }
-
-    Widget makeText(String title, {double width, double height}) {
-        return Container(
-            child: Center(
-                child: Text(
-                    title,
-                    style: TextStyle(
-                        fontSize: 21,
-                        color: Colors.red,
-                    ),
-                ),
-            ),
-            width: width,
-            height: height,
-            decoration: BoxDecoration(color: Colors.green[300]),
-            margin: EdgeInsets.all(10.0),
-        );
-    }
 }
