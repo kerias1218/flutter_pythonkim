de1e8915e795219f2846b74e520cd2e3c99d5622
asdf <asdf@asdf.com>
2019-09-16 14:56:00 +0900

    텍스트 스타일 박스(수평,수직) 가운데 정렬

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,34 @@ void main() {
     ));
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        // TODO: implement build
         return Scaffold(
-            appBar: AppBar(title: Text('텍스트 스타일')),
+            appBar: AppBar(title: Text('텍스트 스타일 박스'),),
             body: Center(
-                child: Text(
-                    '텍스트 스타일',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 42,
-                        color: Colors.blue,
+                child: Container(
+                    child: Text(
+                        '텍스트 스타일!',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 41,
+                            color: Colors.blue,
+                        ),
+                        textAlign: TextAlign.center,
+
+
                     ),
+                    decoration: BoxDecoration(
+                        color: Colors.green[300],
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    ),
+                    width: 300.0,
+                    height: 100.0,
+                    alignment: AlignmentDirectional.center
                 ),
-            )
+            ),
         );
     }
-
 }
+
